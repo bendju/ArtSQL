8c7d9ff07442b8e280859f3d2dae8d81e947b619
Takács Bendeguz <bede@ju>
2024-07-11 14:46:53 +0200

    003

---
+++ database/varrible.py
@@ -0,0 +1,16 @@
+import os
+os.system("")
+
+INTEGER = 0
+STRING = ' '
+FLOAT = 0.0
+BOOL = True
+
+
+COLOR = {
+    "HEADER": "\033[95m",
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "END": "\033[0m",
+}
